fbe2ff63c4c1b61ed707527ab8f756497f1f2fbe
ridaDev <kiratterida@gmail.com>
2025-08-07 11:36:47 +0100

    créer model Author

--- src/main/java/org/example/miniprojet1renforcement/Entitys/Author.java
+++ src/main/java/org/example/miniprojet1renforcement/Entitys/Author.java
@@ -1,4 +1,63 @@
 package org.example.miniprojet1renforcement.Entitys;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String biography;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    private List<Book> books;
+
+
+    public Author() {
+    }
+
+    public Author(Long id, String name, String biography, List<Book> books) {
+        this.id = id;
+        this.name = name;
+        this.biography = biography;
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
 }
