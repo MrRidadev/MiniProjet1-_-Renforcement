c0993ce57b3c2454a54c60bb04104b7159b49bfe
ridaDev <kiratterida@gmail.com>
2025-08-07 11:28:15 +0100

    first commit

---
+++ src/main/java/org/example/miniprojet1renforcement/MiniProjet1RenforcementApplication.java
@@ -0,0 +1,13 @@
+package org.example.miniprojet1renforcement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjet1RenforcementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProjet1RenforcementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/miniprojet1renforcement/MiniProjet1RenforcementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.miniprojet1renforcement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjet1RenforcementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
