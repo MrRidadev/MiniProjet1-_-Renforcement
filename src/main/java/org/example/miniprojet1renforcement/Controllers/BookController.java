f167016ad814ad691e623968c89b56260abf5f8d
ridaDev <kiratterida@gmail.com>
2025-08-07 12:18:29 +0100

    créer les functions CRUD de Book en Controllerbook

--- src/main/java/org/example/miniprojet1renforcement/Controllers/BookController.java
+++ src/main/java/org/example/miniprojet1renforcement/Controllers/BookController.java
@@ -1,5 +1,33 @@
 package org.example.miniprojet1renforcement.Controllers;
 
+
+import org.example.miniprojet1renforcement.Entitys.Book;
+import org.example.miniprojet1renforcement.Services.BookService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@RequestMapping("/book")
+@RestController
 public class BookController {
 
+    private BookService bookService;
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/getAllBook")
+    public List<Book> getAllBook() {
+        return bookService.getAllBooks();
+    }
+    @GetMapping("/getById")
+    public Optional<Book> getBookById(@PathVariable Long id) {
+        return bookService.getBookById(id);
+    }
+    @GetMapping("addBook")
+    public Book addBook(@RequestBody Book book) {
+        return bookService.addBook(book);
+    }
 }
