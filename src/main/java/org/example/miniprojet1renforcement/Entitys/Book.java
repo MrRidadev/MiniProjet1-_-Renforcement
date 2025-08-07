b1edd85d4814545686c8cfef98d1171f9e8ea828
ridaDev <kiratterida@gmail.com>
2025-08-07 11:42:21 +0100

    créer model Book

--- src/main/java/org/example/miniprojet1renforcement/Entitys/Book.java
+++ src/main/java/org/example/miniprojet1renforcement/Entitys/Book.java
@@ -1,4 +1,61 @@
 package org.example.miniprojet1renforcement.Entitys;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String isbn;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    public Book() {
+    }
+
+    public Book(Long id, String title, String isbn, Author author) {
+        this.id = id;
+        this.title = title;
+        this.isbn = isbn;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
 }
